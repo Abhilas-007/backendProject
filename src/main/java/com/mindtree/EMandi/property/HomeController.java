a59ffa11847b65f8ade4494e4589ad9c8f5d3d19
Nusrath Anjum <nusrath.anjum2@mindtree.com>
2021-03-02 22:05:25 +0530

    Commit

--- src/main/java/com/mindtree/EMandi/property/HomeController.java
+++ src/main/java/com/mindtree/EMandi/property/HomeController.java
@@ -1,12 +1,13 @@
 package com.mindtree.EMandi.property;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 	
 	@GetMapping("/")
-	public void get() {
+	public String get() {
+		return "hello";
 	}
 }
