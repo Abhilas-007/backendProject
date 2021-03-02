972db091860a203c16286ec03df8e4d3f5ed226f
Alok Kumar(IN2936) <m1042936@mindtree.com>
2021-03-02 12:09:08 +0530

    commented EMandiApplicationTests.java file.

--- src/test/java/com/example/EMandi/EMandiApplicationTests.java
+++ src/test/java/com/example/EMandi/EMandiApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.EMandi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EMandiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.example.EMandi;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class EMandiApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
