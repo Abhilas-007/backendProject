dbcc3194a2c9378999a744ed04277b36e145ed67
Nusrath Anjum <nusrath.anjum2@mindtree.com>
2021-03-03 18:48:33 +0530

    Commit

--- src/main/java/com/mindtree/EMandi/modules/buyer/controller/BuyerController.java
+++ src/main/java/com/mindtree/EMandi/modules/buyer/controller/BuyerController.java
@@ -1,5 +1,9 @@
 package com.mindtree.EMandi.modules.buyer.controller;
 
-public class BuyerController {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class BuyerController {
+	
+	
 }
--- src/main/java/com/mindtree/EMandi/modules/buyer/dto/BuyerDto.java
+++ src/main/java/com/mindtree/EMandi/modules/buyer/dto/BuyerDto.java
@@ -1,5 +1,52 @@
 package com.mindtree.EMandi.modules.buyer.dto;
 
 public class BuyerDto {
+	
+	private int buyerId;
+	private String buyerName,password,phoneNumber;
+	
+	public BuyerDto() {
+	}
+	
+	public BuyerDto(int buyerId, String buyerName, String password, String phoneNumber) {
+		super();
+		this.buyerId = buyerId;
+		this.buyerName = buyerName;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+	}
 
+	public int getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(int buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public String getBuyerName() {
+		return buyerName;
+	}
+
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
